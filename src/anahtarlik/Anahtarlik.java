f162713ccc027ecd1be14249eff3af5cdb3b3bb3
KutadguCoding <67322550+KutadguCoding@users.noreply.github.com>
2020-06-29 17:51:48 +0300

    Add files via upload

---
+++ src/anahtarlik/Anahtarlik.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package anahtarlik;
+
+/**
+ *
+ * @author m_ugu
+ */
+public class Anahtarlik {
+
+    /**
+     */
+    
+    public static int siteVSkullanici;
+    public static String siteAdi;
+    public static int userNo;
+    
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        new GirisEkrani().setVisible(true);
+        
+    }
+    
+}
